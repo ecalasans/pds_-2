e6fec47e76bcecb015369717969ab49b2924f5ac
Eric <ericcalasans@gmail.com>
2018-11-19 06:34:51 -0300

    Letra b refeita

--- testeNp.py
+++ testeNp.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
+import cmath
 
 DOIS_PI = 2 * np.pi
 PI = 'π'
@@ -15,43 +15,34 @@ def DFT(N=0, freqs=[]):
     '''
 
     # Cálculo da definição da frequência(Omega0)
-    Omega0 = DOIS_PI / (N-1)
+    Omega0 = DOIS_PI / (N)
 
     # Inicializa x[k]
     x_k = np.zeros(N, dtype=np.complex_)
 
+    k = np.arange(0, N)
+
     # Cálculo de x[k]
-    '''for k in range(N):
-        soma = 0
-        for n in range(len(x_n)):
-            expComplexa = np.exp(-1j * Omega0 * k * n)
-            produto = x_n[n] * expComplexa
-            soma = soma + produto
-        x_k[k] = soma
-    '''
+    somaFreq = 0
+    for f in freqs:
+        omega = DOIS_PI * f
+        fator1 = omega - k*Omega0
+        fator2 = omega + k*Omega0
+        frac1 = ((1 - np.exp(1j * N * fator1))/(1 - np.exp(1j * fator1)))
+        frac2 = ((1 - np.exp(-1j * N * fator2))/(1 - np.exp(-1j * fator2)))
 
-    '''
-    for k in range(N-1):
-        soma = 0
-        for f in freqs:
-            omega = DOIS_PI * f
-            valor = (1/2j)*(((1 - np.exp(1j * N * (omega - k*Omega0)))/(1 - np.exp(1j * (omega - k*Omega0))))-\
-                    ((1 - np.exp(-1j * N * (omega + k*Omega0)))/(1 - np.exp(-1j * (omega + k*Omega0)))))
-            soma = soma + valor
-            print('{} - {}:  {}'.format(f, k, soma))
+        somaFreq = somaFreq + ((1/2j) *(frac1 - frac2))
 
-        x_k[k] = soma
-    '''
-
-    for k in range(N):
-        soma = 0
+        x_k = somaFreq
 
     return (x_k, Omega0)
 
 frequencias = [5, 53, 539]
 
 # Cálculo de x1_[k]
-x1_k = DFT(3000, frequencias)
+x1_k = DFT(30000, frequencias)
+
+print(type(x1_k[0]))
 
 k = np.arange(len(x1_k[0]))
 
@@ -59,7 +50,7 @@ kOmega0 = k * x1_k[1]
 locs = np.linspace(0, len(k), 5)
 labels_x = ('0', '{}/2'.format(PI), '{}'.format(PI),'3{}/2'.format(PI), '2{}'.format(PI))
 
-print(locs)
+#print(locs)
 plt.stem(kOmega0, np.abs(x1_k[0]), '-')
 plt.xticks(np.linspace(0,6,5), labels_x)
 plt.subplots_adjust(right=3, top=2)
